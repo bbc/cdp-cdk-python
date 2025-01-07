0262a6d27a2671574d3e99ef43db72c61e162328
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-07 20:42:38 +0000

    Update cdp_cdk_python_stack.py

--- cdp_cdk_python/cdp_cdk_python_stack.py
+++ cdp_cdk_python/cdp_cdk_python_stack.py
@@ -101,13 +101,13 @@ class CdpCdkPythonStack(Stack):
         # )
 
         print('lambda path: %s' % os.path.join(cwd, "lambda_handler.zip")) 
-        print('lambda path: %s' % os.path.join(dirname, "./lambda_handler")) 
+        print('lambda path: %s' % os.path.join(dirname, "lambda_handler")) 
 
         fn = _lambda.Function(
             self, 
             "MyFunction",
             runtime=_lambda.Runtime.PYTHON_3_9,
-            handler="index.handler",
+            handler="lambda_function.lambda_handler",
             timeout=core.Duration.minutes(15),
             # memorySize=1024,
             environment={
