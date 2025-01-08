0e33f3718743fa6aef31d532ccb5e7a19268a127
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-08 12:14:18 +0000

    Update cdp_cdk_python_stack.py

--- cdp_cdk_python/cdp_cdk_python_stack.py
+++ cdp_cdk_python/cdp_cdk_python_stack.py
@@ -116,7 +116,7 @@ class CdpCdkPythonStack(Stack):
                 "REGION": core.Stack.region,
                 # "AVAILABILITY_ZONES": json.dumps(core.Stack.availability_zones),
             },
-            code=_lambda.Code.from_asset("./cdp_cdk_python/lambda_function.py.zip")
+            code=_lambda.Code.from_asset("./cdp_cdk_python")
             # code=_lambda.InlineCode("foo"),
         )
         
