962bb8236a4204488d8dc589c09783682bd6577f
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-09 11:43:45 +0000

    Update policy_loader.py

--- cdp_cdk_python/policy_loader.py
+++ cdp_cdk_python/policy_loader.py
@@ -38,13 +38,13 @@ class PolicyLoader:
         :param policy_name: Name of the policy.
         :param policy_document: The IAM PolicyDocument to attach.
         """
-        # try:
-        role.attach_inline_policy(iam.Policy(
+        try:
+            role.attach_inline_policy(iam.Policy(
                     self,
                     policy_name,
                     document=policy_document
                 )
-        )
-        # except Exception as e:
-        #     print(f"Error: {e}")
+            )
+        except Exception as e:
+            print(f"Error: {e}")
         # role.inline_policies[policy_name] = policy_document
