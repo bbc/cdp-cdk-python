63d39c511628c6bbdfc7bb527fe9a2dec3d6963f
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-10 18:35:00 +0000

    Update lambda_role_policy_stack.py

--- cdp_cdk_python/lambda_role_policy_stack.py
+++ cdp_cdk_python/lambda_role_policy_stack.py
@@ -40,7 +40,7 @@ class LambdaRolePolicyStack(Stack):
 
         get_secret_value_doc = policy_loader.load_policy(
             file_name="get_secret_value.json",
-            variables={"SecretArn":secret_arn.value_as_string}
+            variables={"SecretArn":"arn:aws:secretsmanager:eu-west-1:977228593394:secret:redshift-int-scv-redshift-pii-redshiftcluster-11epfp2gjslrr-scvpiiadmin-VeQ6oT"}#secret_arn.value_as_string
         )
 
         describe_statement_doc = policy_loader.load_policy(
