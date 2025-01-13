d6565815a847f5bf00dc552ce7e5d92bd179661d
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-13 15:58:34 +0000

    Update lambda_role_policy_stack.py

--- cdp_cdk_python/lambda_role_policy_stack.py
+++ cdp_cdk_python/lambda_role_policy_stack.py
@@ -45,10 +45,10 @@ class LambdaRolePolicyStack(Stack):
 
         accout_id = os.getenv('CDK_DEFAULT_ACCOUNT')
         region = os.getenv('CDK_DEFAULT_REGION')
-        execute_batch_statement_doc = policy_loader.load_policy(
-            file_name="execute_batch_statement.json",
-            replacements={"ClusterName":cluster_name.value_as_string, "AWS::Region":region, "AWS::AccountId":accout_id}
-        )
+        # execute_batch_statement_doc = policy_loader.load_policy(
+        #     file_name="execute_batch_statement.json",
+        #     replacements={"ClusterName":cluster_name.value_as_string, "AWS::Region":region, "AWS::AccountId":accout_id}
+        # )
 
         describe_statement_doc = policy_loader.load_policy(
             file_name="describe_statement.json",
