f9f750f87b0d1d60640fce28e099632ff9d47e84
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-13 16:12:34 +0000

    Update lambda_role_policy_stack.py

--- cdp_cdk_python/lambda_role_policy_stack.py
+++ cdp_cdk_python/lambda_role_policy_stack.py
@@ -22,6 +22,8 @@ class LambdaRolePolicyStack(Stack):
         parameter_loader = CfnParameterLoader(self, 'cdp_cdk_python/params/cdp-pii-datashare.json')
         cluster_name = parameter_loader.get_parameter("ClusterName")
         secret_arn = parameter_loader.get_parameter("SecretArn")
+        print(cluster_name)
+        print(secret_arn)
         
         # Create an IAM Role
         iam_role = iam.Role(
