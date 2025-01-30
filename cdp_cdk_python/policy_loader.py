b6479aa6c55ce88d680b50aa61e0df56d2001d4b
Imtiah01 <hazzaz.imtiaz.ext@bbc.co.uk>
2025-01-30 15:37:32 +0000

    Update policy_loader.py

--- cdp_cdk_python/policy_loader.py
+++ cdp_cdk_python/policy_loader.py
@@ -14,6 +14,8 @@ class PolicyLoader:
     def load_policy(self, file_name: str, replacements: dict) -> iam.PolicyDocument:
         policy_json = self._do_replace(file_name, replacements) 
         print(str(policy_json))
+        policy_json = policy_json.replace("'","\"")
+        print(str(policy_json))
         # policy_json = re.sub(
         #         r"\{'Fn::Sub':",
         #         '{"Fn::Sub":',
